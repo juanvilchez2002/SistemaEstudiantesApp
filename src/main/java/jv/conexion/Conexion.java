b5bcf93276c3d46ffe995cb3a51ef054549f6923
Juan Vilchez <juanvilchez2002@gmail.com>
2024-03-30 22:55:12 -0500

    adicionando la clase Conexion

--- src/main/java/jv/conexion/Conexion.java
+++ src/main/java/jv/conexion/Conexion.java
@@ -1,6 +1,7 @@
 package jv.conexion;
 
 import java.sql.Connection;
+import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class Conexion {
@@ -12,7 +13,7 @@ public class Conexion {
 
         //variables de conexion
         var baseDatos = "estudiantes_db";//nombre de la bd
-        var url = "jdbc:mysql://localhost:3306/"+baseDatos;//dirección de la db
+        var url = "jdbc:mysql://localhost:3306/" + baseDatos;//dirección de la db
         var usuario ="root";
         var pass ="root";
 
@@ -20,11 +21,19 @@ public class Conexion {
            //cargamos la clase del driver de mysql en memoria Driver
            Class.forName("com.mysql.cj.jdbc.Driver");
 
-           conexion.DriverManager.getConnection(url, usuario, pass);
+           conexion = DriverManager.getConnection(url, usuario, pass);
        }catch (ClassNotFoundException | SQLException e){
            System.out.println("Ocurrio un error en la conexion: "+e.getMessage());
        }
 
         return conexion;
+    }
+
+    public static void main(String[] args) {
+        var conexion = Conexion.getConexion();
+        if(conexion != null)
+            System.out.println("Conexión exitosa: "+conexion);
+        else
+            System.out.println("Error al conectarse");
     }
 }
