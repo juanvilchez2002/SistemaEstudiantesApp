6b5cc0e9a4047a29dbd538a1e18003abf41dc5d3
Juan Vilchez <juanvilchez2002@gmail.com>
2024-03-30 22:43:06 -0500

    Inicializando el proyecto

---
+++ src/main/java/jv/conexion/Conexion.java
@@ -0,0 +1,30 @@
+package jv.conexion;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+
+public class Conexion {
+
+    //es estatico xq se ha de llamar desde otras clases sin crear una instancia
+    //retorna una valor de tipo Connection
+    public static Connection getConexion(){
+        Connection conexion = null;
+
+        //variables de conexion
+        var baseDatos = "estudiantes_db";//nombre de la bd
+        var url = "jdbc:mysql://localhost:3306/"+baseDatos;//dirección de la db
+        var usuario ="root";
+        var pass ="root";
+
+       try{
+           //cargamos la clase del driver de mysql en memoria Driver
+           Class.forName("com.mysql.cj.jdbc.Driver");
+
+           conexion.DriverManager.getConnection(url, usuario, pass);
+       }catch (ClassNotFoundException | SQLException e){
+           System.out.println("Ocurrio un error en la conexion: "+e.getMessage());
+       }
+
+        return conexion;
+    }
+}
